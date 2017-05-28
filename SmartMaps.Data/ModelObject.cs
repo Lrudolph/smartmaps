fe825e8d887185286437ccbc452e9f8211438b51
Linda Rudolph <s75161@htw-dresden.de>
2017-05-28 17:58:11 +0200

    ObservableLists Databinding

--- SmartMaps.Data/ModelObject.cs
+++ SmartMaps.Data/ModelObject.cs
@@ -4,8 +4,13 @@ namespace SmartMaps.Data
 {
     public abstract class ModelObject
     {
-        public String IDname { get; private set; }
+        public String IDname { get; set; } = "TestModelObject";
         public String soundFileName { get; private set; }
         public int TipToiCodeID { get; private set; }
+
+        public override string ToString()
+        {
+            return IDname;
+        }
     }
 }
